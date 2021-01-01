1af424f20db28a04f34a0cd87e5dd42b684dd87b
Yaroslav Kravets <yar.kravets@ukr.net>
2021-01-01 23:07:31 +0200

    Lesson5

---
+++ Lesson5/ScreenshotClient/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenshotClient
+{
+    public partial class MainWindow : Window
+    {
+        private const int Port = 2020;
+        private const int MillisecondsTimeout = 3000;
+        private const int BlockSize = 8096;
+        private const int TimeoutWithoutPackets = 50;
+        static string host = "127.0.0.1";
+
+        bool sendScreenShot = false;
+        UdpClient udpClient = null;
+        string filename;
+        string fullPath;
+        int counter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Start.
+            sendScreenShot = true;
+
+            udpClient = new UdpClient();
+            udpClient.Connect(host, Port);
+
+            Task task = new Task(SendPicture);
+            task.Start();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Stop.
+            sendScreenShot = false;
+
+            if (udpClient != null)
+            {
+                udpClient.Close();
+                udpClient = null;
+            }
+        }
+
+        void SendPicture()
+        {
+            do
+            {
+                filename = $"ScreenCapture- { DateTime.Now.ToString("ddMMyyyy-hhmmss") }.png";
+                fullPath = Directory.GetCurrentDirectory() + "\\screenshots\\" + filename;
+
+                using (Bitmap bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                            Screen.PrimaryScreen.Bounds.Height))
+                using (Graphics g = Graphics.FromImage(bmpScreenCapture))
+                {
+                    g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                     Screen.PrimaryScreen.Bounds.Y,
+                                     0, 0,
+                                     bmpScreenCapture.Size,
+                                     CopyPixelOperation.SourceCopy);
+                    bmpScreenCapture.Save(fullPath);
+                }
+
+                // Send file to server.
+                var fileNameByte = Encoding.UTF8.GetBytes(filename);
+                udpClient.Send(fileNameByte, fileNameByte.Length);
+
+                FileInfo fileInfo = new FileInfo(fullPath);
+                var fileSize = fileInfo.Length;
+                var FileSizeInBytes = Encoding.UTF8.GetBytes(fileSize.ToString());
+
+                udpClient.Send(FileSizeInBytes, FileSizeInBytes.Length);
+
+                var binaryFile = File.ReadAllBytes(fullPath);
+
+                byte[] buffer;
+                for (int i = 0; i < binaryFile.Length;)
+                {
+                    int k = BlockSize < (binaryFile.Length - i) ? BlockSize : binaryFile.Length - i;
+
+                    if (k != BlockSize)
+                    {
+
+                    }
+                    buffer = binaryFile.Skip(i).Take(k).ToArray();
+                    udpClient.Send(buffer, buffer.Length);
+
+                    i += k;
+
+                    Thread.Sleep(TimeoutWithoutPackets);
+                }
+
+                counter++;
+
+                Dispatcher.Invoke(() => count.Content = counter);
+
+                // Remove sended file.
+                File.Delete(fullPath);
+
+                // Sleep.
+                Thread.Sleep(MillisecondsTimeout);
+
+            } while (sendScreenShot);
+        }
+    }
+}
---
+++ Lesson5/ScreenshotReader/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenshotReader
+{
+    class Program
+    {
+        const int PORT = 2020;
+
+        private static string directoryPath = null;
+
+        static void Main(string[] args)
+        {            
+            Console.Title = "Server:";
+
+            if (!Directory.Exists("screens"))
+            {
+                Directory.CreateDirectory("screens");
+                Console.WriteLine("Directory \"screens\" created success");
+            }
+
+            directoryPath = Directory.GetCurrentDirectory() + "\\screens";
+
+            Console.WriteLine("Wait...");
+
+            var server = new UdpClient(PORT);
+            try
+            {
+                IPEndPoint ep = null;
+
+                while (true)
+                {
+                    var data = server.Receive(ref ep);
+                    var fileName = Encoding.UTF8.GetString(data);
+                    Console.WriteLine($"Filename: {fileName}");
+
+                    data = server.Receive(ref ep);
+                    var fileSize = Int32.Parse(Encoding.UTF8.GetString(data));
+                    
+                    Console.WriteLine($"Size: {fileSize}");
+                    Console.WriteLine($"Need packets: {fileSize / 8096.0 }");
+
+                    var buffer = new byte[fileSize];
+
+                    int offset = 0;
+                    int packet = 0;
+                    do
+                    {
+                        data = server.Receive(ref ep);
+                        for (int j = 0; j < data.Length; j++)
+                        {
+                            try
+                            {
+                                buffer[j + offset] = data[j];
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }                            
+                        }
+
+                        offset += data.Length;
+                        Console.WriteLine($"Offset: {offset}");
+                        Console.WriteLine($"Packet: {packet + 1}");
+                        Console.WriteLine($"Packet size: {data.Length}");
+
+                        packet++;
+
+                    } while (fileSize > offset);
+
+
+                    File.WriteAllBytes(directoryPath + "\\" + fileName, buffer);
+
+                    Console.WriteLine("Received ok..");
+                }
+            }
+            catch (SocketException e)
+            {
+                server.Close();
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
